c7490a8c879789d00f6b0a8bb54be5440ecd1ee2
SeanLiao <liaoshengyi0@gmail.com>
2026-03-14 20:44:12 +0800

    delete demo

--- lsy_demo/etf_rotation_backtest/run_etf_rotation.sh
+++
@@ -1,9 +0,0 @@
-cd /home/lsy/data/project/Quant/qteasy/lsy_demo
-
-python etf_rotation_backtest.py \
-  --asset_pool 518880.SH,513100.SH,159915.SZ,510180.SH,513130.SH,517000.SH,512100.SH,000300.SH,588000.SH,588750.SH \
-  --start_date 20200101 \
-  --end_date 20251231 \
-  --max_sel 1 \
-  --benchmark_asset 000300.SH
-
